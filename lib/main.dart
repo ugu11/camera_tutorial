4fb5a28b644789b265144817d665dca9dc5681e6
Hugo Gomes <hugogomes@MBP-de-Hugo-2.lan>
2020-05-13 22:06:02 +0100

    Device detection before converting image fixed

--- lib/main.dart
+++ lib/main.dart
@@ -95,34 +95,45 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-          // Allocate memory for the 3 planes of the image
-          Pointer<Uint8> p = allocate(count: _savedImage.planes[0].bytes.length);
-          Pointer<Uint8> p1 = allocate(count: _savedImage.planes[1].bytes.length);
-          Pointer<Uint8> p2 = allocate(count: _savedImage.planes[2].bytes.length);
+          imglib.Image img;
 
-          // Assign the planes data to the pointers of the image
-          Uint8List pointerList = p.asTypedList(_savedImage.planes[0].bytes.length);
-          Uint8List pointerList1 = p1.asTypedList(_savedImage.planes[1].bytes.length);
-          Uint8List pointerList2 = p2.asTypedList(_savedImage.planes[2].bytes.length);
-          pointerList.setRange(0, _savedImage.planes[0].bytes.length, _savedImage.planes[0].bytes);
-          pointerList1.setRange(0, _savedImage.planes[1].bytes.length, _savedImage.planes[1].bytes);
-          pointerList2.setRange(0, _savedImage.planes[2].bytes.length, _savedImage.planes[2].bytes);
-          
-          // Call the convertImage function and convert the YUV to RGB
-          Pointer<Uint32> imgP = conv(p, p1, p2, _savedImage.planes[1].bytesPerRow,
-            _savedImage.planes[1].bytesPerPixel, _savedImage.width, _savedImage.height);
+          if(Platform.isAndroid){
+            // Allocate memory for the 3 planes of the image
+            Pointer<Uint8> p = allocate(count: _savedImage.planes[0].bytes.length);
+            Pointer<Uint8> p1 = allocate(count: _savedImage.planes[1].bytes.length);
+            Pointer<Uint8> p2 = allocate(count: _savedImage.planes[2].bytes.length);
+
+            // Assign the planes data to the pointers of the image
+            Uint8List pointerList = p.asTypedList(_savedImage.planes[0].bytes.length);
+            Uint8List pointerList1 = p1.asTypedList(_savedImage.planes[1].bytes.length);
+            Uint8List pointerList2 = p2.asTypedList(_savedImage.planes[2].bytes.length);
+            pointerList.setRange(0, _savedImage.planes[0].bytes.length, _savedImage.planes[0].bytes);
+            pointerList1.setRange(0, _savedImage.planes[1].bytes.length, _savedImage.planes[1].bytes);
+            pointerList2.setRange(0, _savedImage.planes[2].bytes.length, _savedImage.planes[2].bytes);
             
-          // Get the pointer of the data returned from the function to a List
-          List imgData = imgP.asTypedList((_savedImage.width * _savedImage.height));
-          // Generate image from the converted data  
-          imglib.Image img = imglib.Image.fromBytes(_savedImage.height, _savedImage.width, imgData);
-          
-          // Free the memory space allocated
-          // from the planes and the converted data
-          free(p);
-          free(p1);
-          free(p2);
-          free(imgP);
+            // Call the convertImage function and convert the YUV to RGB
+            Pointer<Uint32> imgP = conv(p, p1, p2, _savedImage.planes[1].bytesPerRow,
+              _savedImage.planes[1].bytesPerPixel, _savedImage.planes[0].bytesPerRow, _savedImage.height);
+              
+            // Get the pointer of the data returned from the function to a List
+            List imgData = imgP.asTypedList((_savedImage.planes[0].bytesPerRow * _savedImage.height));
+            // Generate image from the converted data  
+            img = imglib.Image.fromBytes(_savedImage.height, _savedImage.planes[0].bytesPerRow, imgData);
+            
+            // Free the memory space allocated
+            // from the planes and the converted data
+            free(p);
+            free(p1);
+            free(p2);
+            free(imgP);
+          }else if(Platform.isIOS){
+            img = imglib.Image.fromBytes(
+              _savedImage.planes[0].bytesPerRow,
+              _savedImage.height,
+              _savedImage.planes[0].bytes,
+              format: imglib.Format.bgra,
+            );
+          }
 
           Navigator.push(context, MaterialPageRoute(builder: 
             (context) => new ImagePreview(img: img)));
